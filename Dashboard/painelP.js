ca2c23004c10dfbeca084aab2983d60ec4f8bb25
gabriella <gabriella.inacio@sptech.school>
2023-05-23 05:30:39 -0300

    dash em andamento - painelP

---
+++ Dashboard/painelP.js
@@ -0,0 +1,20 @@
+ //variaveis globais 
+ var contador= 0;
+
+ //funcoes do menu de nav
+ function clickmenu() {
+ contador+=1
+ 
+ if(contador>0){
+ if(contador==1){
+     mensagem.innerHTML= `<ul class="menu_resp"> 
+         <li><a href=" ">Sala A</a></li>
+         <li><a href=" ">Sala B</a></li>
+         <li><a href=" ">Sala C</a></li>`
+ 
+ } else {
+     mensagem.innerHTML= ' ';
+     contador=0
+ }
+ } 
+ } 
