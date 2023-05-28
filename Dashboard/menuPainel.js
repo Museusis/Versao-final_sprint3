8c9d7c4dffc9f70753ce63b17334605143e57482
gabriella <gabriella.inacio@sptech.school>
2023-05-28 16:35:10 -0300

    arrumei a altura dos itens header

--- Dashboard/menuPainel.js
+++ Dashboard/menuPainel.js
@@ -8,9 +8,9 @@ contador+=1
 if(contador>0){
 if(contador==1){
     mensagem.innerHTML= `<ul class="menu_resp"> 
-        <li><a href=" ">Sala A</a></li>
-        <li><a href="dashboard.html">Sala B</a></li>
-        <li><a href=" ">Sala C</a></li>`
+        <li><a href="salaA.html">Sala A</a></li>
+        <li><a href="salaB.html">Sala B</a></li>
+        <li><a href="salaC.html">Sala C</a></li>`
 
 } else {
     mensagem.innerHTML= ' ';
@@ -18,7 +18,3 @@ if(contador==1){
 }
 } 
 } 
-
-function verSala(){
-   
-}
