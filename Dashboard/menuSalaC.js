269b1173098017e85a8473313b7a9df7a607cd0e
gabriella <gabriella.inacio@sptech.school>
2023-05-28 16:43:00 -0300

    ajuste nos menus

---
+++ Dashboard/menuSalaC.js
@@ -0,0 +1,20 @@
+//variaveis globais 
+var contador= 0;
+
+//funcoes do menu de nav
+function clickmenu() {
+contador+=1
+
+if(contador>0){
+if(contador==1){
+    mensagem.innerHTML= `<ul class="menu_resp"> 
+        <li><a href="painel_principal.html">Painel</a></li>
+        <li><a href="salaA.html">Sala A</a></li>
+        <li><a href="salaB.html">Sala B</a></li>`
+
+} else {
+    mensagem.innerHTML= ' ';
+    contador=0
+}
+} 
+} 
